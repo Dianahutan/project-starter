4197d1d0f46ff96ffc2de810c9cfaa5e2adbdc84
Diana Hutan <dianahutange@gmail.com>
2023-03-06 15:19:56 +0200

    index.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const primaryNav = document.querySelector(".primary_nav");
-const navToggle = document.querySelector(".mobile_nav");
-
-navToggle.addEventListener("click", ( ) => {
-  const visibility = primaryNav.getAttribute("data-visible");
-
-  if (visibility === "false") {
-    primaryNav.setAttribute("data-visible", true);
-    navToggle.setAttribute("aria-expanded", true);
-  }
-  if (visibility === "true") {
-		 console.log(visibility)
-    primaryNav.setAttribute("data-visible", false);
-    navToggle.setAttribute("aria-expanded", false);
-  }
-})
+function toggleMenu() {
+  document.getElementById("primary_nav").classList.toggle("active");
+}
