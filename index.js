aa34d62d76c34111dde8310f00fa1aeb626312be
Diana Hutan <dianahutange@gmail.com>
2023-03-06 15:41:02 +0200

    unresponsive menu

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function toggleMenu() {
-  document.getElementById("primary_nav").classList.toggle("active");
-}
+const primaryNav = document.querySelector(".primary_nav")
+const navToggle = document.querySelector(".mobile_nav")
+
+navToggle.addEventListener("click", () => {
+	const visibility = primaryNav.getAttribute("data-visible")
+
+	if (visibility === "false") {
+		primaryNav.setAttribute("data-visible", true);
+		navToggle.setAttribute("aria-expanded", true);
+	}
+	else if (visibility === "true") {
+		primaryNav.setAttribute("data-visible", false);
+		navToggle.setAttribute("aria-expanded", false);
+	}
+})
